ea74081bdda688bdbb6f5033b38f03831c3df929
wjhln <wjh.ln@outlook.com>
2023-07-14 21:39:54 +0800

    增加类似 printf 的控制台输出

--- HMI/console_uart.h
+++ HMI/console_uart.h
@@ -9,12 +9,14 @@ class Console_t
 		uint8_t rev_buff[256];
 		UART_HandleTypeDef *huart_m;
 		Console_t(UART_HandleTypeDef *huart);
-		void Send(const char * message);
+		void Send(const char *str, ...);
 		void Open(void);
 		void Receive(uint16_t size);
 		void SendInt(int num);
 		void SendFloat(float num);
 		void SendThreeFloat(float num1,float num2,float num3);
 };
+
+uint16_t _pow(int x, int y);
 
 #endif
