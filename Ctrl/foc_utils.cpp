339de5560326243668d7d283f4ccc1adc308cd2b
wjhln <wjh.ln@outlook.com>
2023-07-24 22:55:15 +0800

    增加取值函数

--- Ctrl/foc_utils.cpp
+++ Ctrl/foc_utils.cpp
@@ -7,17 +7,18 @@ uint32_t getMicros(void)
 {
     HAL_TIM_Base_Stop(&htim4);
     uint32_t count = __HAL_TIM_GET_COUNTER(&htim4);
-    __HAL_TIM_SET_COUNTER(&htim4,0);
+    __HAL_TIM_SET_COUNTER(&htim4, 0);
     HAL_TIM_Base_Start(&htim4);
     return count;
 }
 
 float _normalizeAngle(float angle)
 {
-  float a = fmod(angle, _2PI);
-  return a >= 0 ? a : (a + _2PI);
+    float a = fmod(angle, _2PI);
+    return a >= 0 ? a : (a + _2PI);
 }
 
-float _electricalAngle(float shaft_angle, int pole_pairs) {
-  return (shaft_angle * pole_pairs);
+float _electricalAngle(float shaft_angle, int pole_pairs)
+{
+    return (shaft_angle * pole_pairs);
 }
--- Ctrl/foc_utils.h
+++ Ctrl/foc_utils.h
@@ -1,11 +1,11 @@
 #ifndef FOC_UTILS_H
 #define FOC_UTILS_H
+#include "math.h"
+#include "stdint.h"
 
-#include<stdint.h>
-
-
+#define _3PI_2 4.71238898038f
 #define _2PI 6.28318530718f
-#define _constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define _constrain(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
 
 struct DQVoltage_s
 {
@@ -13,10 +13,8 @@ struct DQVoltage_s
     float q;
 };
 
-
 uint32_t getMicros(void);
 float _normalizeAngle(float angle);
 float _electricalAngle(float shaft_angle, int pole_pairs);
-
 
 #endif
