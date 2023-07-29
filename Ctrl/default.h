bc4d8598248935dd301f3f461e10b11ef9be14f6
wjhln <wjh.ln@outlook.com>
2023-07-30 00:18:37 +0800

    add default constants

---
+++ Ctrl/default.h
@@ -0,0 +1,14 @@
+#ifndef __DEFAULT_H__
+#define __DEFAULT_H__
+
+#define DEF_POWER_SUPPLY 12.0f 
+
+#define DEF_VEL_FILTER_Tf 0.05f
+#define DEF_ANGLE_FILTER_Tf 0.0f
+
+#define DEF_PID_VEL_P 0.3f
+#define DEF_PID_VEL_I 0.0f
+#define DEF_PID_VEL_D 0.0f
+#define DEF_PID_VEL_LIMIT (DEF_POWER_SUPPLY)
+
+#endif
