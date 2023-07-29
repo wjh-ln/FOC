63aa98a2b9bc1a94f4c731c14c991ce7be1b77ea
wjhln <wjh.ln@outlook.com>
2023-07-30 00:25:20 +0800

    velocity close control

--- User/user.cpp
+++ User/user.cpp
@@ -1,20 +1,32 @@
 #include "user.h"
 #include "common.h"
 #include "tim.h"
+#include "as5600.h"
 
-LED_t LED(LED_GPIO_Port,LED_Pin,LOW);
-Console_t Console(&huart1);
+
+LED_t LED(LED_GPIO_Port, LED_Pin, LOW);
+Console_t console(&huart1);
 BLDCMotor Motor;
+AS5600 encoder;
 
 void Main(void)
 {
-    Console.Open();
-    Motor.driverInit();
     Motor.voltage_power_supply = 11.1;
-    Motor.voltage_limit = 8;
+    Motor.voltage_limit = 10;
+    Motor.pole_pairs = 7;
+    Motor.controller = velocity;
+    Motor.PID_velocity.output_limit = 10;
+    microsInit();
+    console.Open();
+
+    Motor.driverInit();
+    Motor.linkSensor(encoder);
+    Motor.initFOC();
+
     while (1)
     {
-        Motor.move(0.5);
-        LED.Toggle();
+        Motor.move(10);
+        console.SendFloat(encoder.getSensorVelocity());
     }
 }
+
